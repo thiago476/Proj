f1febc19a3e2829b24c114688f534205145eaa34
thiago dias <thiago.gdias5@gmail.com>
2023-09-12 21:45:34 -0300

    alteracao

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('ok');
+const form = document.getElementById('form')
+const username = document.getElementsByld('username')
+const emailinput = document.querySelector("#name")
+const messageTextarea = document.querySelector("message")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
